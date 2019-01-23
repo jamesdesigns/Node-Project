46dff28aa608745844927146e50c59798457d27a
James Hooper <jameshooperdesigns@gmail.com>
2019-01-23 15:03:10 -0700

    Add controller file

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const product = require('./routes/product.route')
+
 const app = express()
+
+app.use('/products', product)
 
 const port = 5775
 
@@ -9,3 +13,4 @@ app.listen(port, () => {
     console.log(`Server is up and running on ${port}`)
 })
 
+// CRUD - create. read. update. delete
---
+++ controllers/product.controller.js
@@ -0,0 +1,6 @@
+const Product = require('../models/product.model')
+
+exports.test = (req, res, next) => {
+    res.send('Greetings from the test controller!')
+}
+
